ee3d9e66d9120372c7bacf6887051caf14666910
XNTEABDSC <2364400855@qq.com>
2025-05-02 13:32:22 +0800

    v0.9.0.1

--- _modinfo.lua
+++ _modinfo.lua
@@ -1,13 +1,13 @@
 return {
-	version="v0.0.0.7",
-	shortGame="ZK",
-	description="later",
 	mutator="zk but more api",
+	name="ZK Modders Dream",
 	depend={
 		[1]="Zero-K v1.13.4.3",
 	},
-	name="ZK Modders Dream",
-	modtype=1,
 	shortname="ZKMD",
+	modtype=1,
+	shortGame="ZK",
+	version="v0.0.0.8",
+	description="later",
 	game="Zero-K",
 }
