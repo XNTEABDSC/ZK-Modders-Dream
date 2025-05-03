91b7c339a32bf6d3120e53f90307f47ab38f4961
XNTEABDSC <2364400855@qq.com>
2025-05-04 03:17:58 +0800

    v0.0.0.13 movedef log

--- _modinfo.lua
+++ _modinfo.lua
@@ -1,13 +1,13 @@
 return {
-	shortGame="ZK",
 	modtype=1,
-	name="ZK Modders Dream",
+	shortGame="ZK",
 	shortname="ZKMD",
+	game="Zero-K",
+	description="later",
+	name="ZK Modders Dream",
+	version="v0.0.0.12",
+	mutator="zk but more api",
 	depend={
 		[1]="Zero-K v1.13.4.3",
 	},
-	mutator="zk but more api",
-	version="v0.0.0.11",
-	game="Zero-K",
-	description="later",
 }
