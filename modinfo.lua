8b40e0d56d666db7661cd4f4412174a2df2d4d86
XNTEABDSC <2364400855@qq.com>
2025-05-10 22:21:56 +0800

    v0.0.0.14

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 return {
 	name='_ZK_Modders_Dream_dev',
 	description='later',
-	version='v0.0.0.13',
+	version='v0.0.0.14',
 	shortname='ZKMD',
 	mutator='zk but more api',
 	game='Zero-K',
 	shortGame='ZK',
 	modtype=1,
 	depend = {
-		-- [[rapid://zk:stable]],
-		[[Zero-K v1.13.4.3]]
+		[[rapid://zk:stable]],
+		-- [[Zero-K v1.13.4.3]]
 	},
 }
