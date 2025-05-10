a69a05cd16e90f82406023ba80bbb08c6f12aa28
XNTEABDSC <2364400855@qq.com>
2025-05-10 22:38:48 +0800

    v0.0.0.15

--- _modinfo.lua
+++ _modinfo.lua
@@ -1,13 +1,13 @@
 return {
 	description="later",
-	game="Zero-K",
-	version="v0.0.0.13",
-	shortname="ZKMD",
 	name="ZK Modders Dream",
 	shortGame="ZK",
+	mutator="zk but more api",
 	modtype=1,
 	depend={
 		[1]="Zero-K v1.13.4.3",
 	},
-	mutator="zk but more api",
+	game="Zero-K",
+	shortname="ZKMD",
+	version="v0.0.0.14",
 }
--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 return {
 	name='_ZK_Modders_Dream_dev',
 	description='later',
-	version='v0.0.0.14',
+	version='v0.0.0.15',
 	shortname='ZKMD',
 	mutator='zk but more api',
 	game='Zero-K',
